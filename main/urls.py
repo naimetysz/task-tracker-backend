1501d4c60e9eaf3320d05569ae798b54917b996d
naimetysz <nnaimetuysuz@gmail.com>
2023-06-02 12:31:46 +0300

    last

--- main/urls.py
+++ main/urls.py
@@ -19,3 +19,15 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# MVS - Router
+from rest_framework import routers
+from todo.views import TodoView
+
+router = routers.DefaultRouter()
+router.register('todo', TodoView)
+
+# urlpatterns += [
+#     path('', router.urls),
+# ]
+
+urlpatterns += router.urls

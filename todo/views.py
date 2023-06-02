6243f6533bd443dfd3bf014392b977c1223b4b91
naimetysz <nnaimetuysuz@gmail.com>
2023-06-02 12:45:56 +0300

    up

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,8 @@
-#from django.shortcuts import render
+#from django.shortcuts import rende
 from rest_framework.viewsets import ModelViewSet
-from serializers import Todo,TodoSerializer
+
+from .serializers import Todo, TodoSerializer
+
 class TodoView(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-
-
-# Create your views here.
